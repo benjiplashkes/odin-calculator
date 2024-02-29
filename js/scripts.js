9e081f61e5191c5e5ea6fcb2680bf1bbd836dea4
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-02-29 13:11:45 +0200

    Add basic functionality
    the calculator works with two number input
    
    bugs exist when entering more then two numbers, and more then one
    operator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,129 @@
 // Cache elements
-const resultDiv = document.querySelector('.result');
-const inputButtons = document.querySelectorAll('.inputButton');
+const resultDiv = document.querySelector(".result");
+const inputButtons = document.querySelectorAll(".inputButton");
+
 // Input vars
-let resultDisplay = 123456;
+let resultVar = 0;
+let lastResult = 0;
 let firstEnteredNumber = 0;
 let secondEnteredNumber = 0;
 let enteredOperator = "";
+let isFirstNumberEntered = false;
+
+//Button Event Listeners
+inputButtons.forEach((btn) => btn.addEventListener("click", handleClick));
+
+//Click functions
+function clickedEqual() {
+	//calculate new result
+	const result = operate(
+		firstEnteredNumber,
+		secondEnteredNumber,
+		enteredOperator
+	);
+
+	// save last given result
+	lastResult = resultVar;
+
+	// save current result
+	resultVar = result;
+
+	//reset input vars
+	isFirstNumberEntered = true;
+	firstEnteredNumber = result;
+	secondEnteredNumber = 0;
+	enteredOperator = "";
+
+	// update display
+	clearDisplay();
+	renderDisplay(result);
+}
+function clickedClear() {
+	//reset results
+	lastResult = 0;
+	resultVar = 0;
+	//reset inputs
+	firstEnteredNumber = 0;
+	secondEnteredNumber = 0;
+	enteredOperator = 0;
+	isFirstNumberEntered = false;
+	//clear the display
+	clearDisplay();
+}
+function clickedOperator(value) {
+	enteredOperator = value;
+	isFirstNumberEntered = true;
+    clearDisplay()
+}
+function clickedNumber(value) {
+	console.log("number: ",value)
+    if(!isFirstNumberEntered){
+        firstEnteredNumber === 0 
+        ? firstEnteredNumber = value
+        : firstEnteredNumber += value
+    }else {
+        secondEnteredNumber === 0 
+        ? secondEnteredNumber = value
+        : secondEnteredNumber += value
+    }
+    let number = isFirstNumberEntered ? secondEnteredNumber : firstEnteredNumber
+	renderDisplay(number);
+}
+
+// Click handler
+function handleClick(e) {
+	let value = e.target.dataset.value;
+	console.log(value);
+	if (value === "=") return clickedEqual();
+	if (value === "clear") return clickedClear();
+	if (
+		value === "+" ||
+		value === "-" ||
+		value === "*" ||
+		value === "/" ||
+		value === "**"
+	)
+		return clickedOperator(value);
+	return clickedNumber(value);
+}
 
 //Math Functions
 const add = (number1, number2) => number1 + number2;
-const subtract = (number1, number2) => number1 - number2 ;
-const multiply = (number1, number2) => number1 * number2 ;
-const divide = (number1, number2) => number1 / number2 ;
-const power = (number1, number2) => number1 ** number2 ;
+const subtract = (number1, number2) => number1 - number2;
+const multiply = (number1, number2) => number1 * number2;
+const divide = (number1, number2) => number1 / number2;
+const power = (number1, number2) => number1 ** number2;
 
 //Perform operations
-function operate (number1, number2, operator){
-    clearDisplay()
-    let result = 0
-    switch(operator){
-        case "+":
-            result = add(number1, number2);
-            break;
-        case "-":
-            result =  subtract(number1, number2);
-            break;
-        case "*":
-            result =  multiply(number1, number2);
-            break;
-        case "/":
-            result =  divide(number1, number2);
-            break;
-        case "**":
-            result =  power(number1, number2);
-            break;    
-        default:
-            result =  "Error: Invalid operator input";
-    };
-    return updateResult(result)
-};
-function updateResult(number){
-    resultDisplay += number
-    return resultDiv.textContent = resultDisplay
-};
-function clearDisplay(){
-    resultDisplay = 0
-    return resultDiv.textContent = resultDisplay
-};
+function operate(number1, number2, operator) {
+	number1 = Number(number1);
+	number2 = Number(number2);
+	let result = 0;
+	switch (operator) {
+		case "+":
+			result = add(number1, number2);
+			break;
+		case "-":
+			result = subtract(number1, number2);
+			break;
+		case "*":
+			result = multiply(number1, number2);
+			break;
+		case "/":
+			result = divide(number1, number2);
+			break;
+		case "**":
+			result = power(number1, number2);
+			break;
+	}
+	return result;
+}
 
-
-
-
-console.log(inputButtons);
-clearDisplay()
-updateResult(123456789)
-operate(3,2, "+")
-// operate(3,2, "-")
-// operate(3,2, "*")
-// operate(3,2, "/")
-// operate(3,2, "**")
+//Manage display
+function renderDisplay(number) {
+	return (resultDiv.textContent = number);
+}
+function clearDisplay() {
+	return (resultDiv.textContent = 0);
+}
+clickedClear();
