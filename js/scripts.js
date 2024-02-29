69c8d2c22f63f821f60d22a35d85afcf372c7a7b
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-02-29 14:26:32 +0200

    Add keyboard input
    add keyboard input to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,25 @@ let secondEnteredNumber = 0;
 let enteredOperator = "";
 let isFirstNumberEntered = false;
 
-//Button Event Listeners
-inputButtons.forEach((btn) => btn.addEventListener("click", handleClick));
+// array of available operators
+const operators = ["+", "-", "*", "/", "**"];
 
-//Click functions
-function clickedEqual() {
+// Event Listeners
+inputButtons.forEach((btn) => btn.addEventListener("click", handleInput));
+document.addEventListener("keyup", handleInput);
+
+// Click handler
+function handleInput(e){
+    let value = ""
+    if(e.type === "click") value = e.target.dataset.value
+    if(e.type === "keyup") value = e.key
+	if (value === "=" || value === "Enter") return inputEqual();
+	if (value === "Escape" || value === "Backspace") return inputClear();
+	if (operators.includes(value)) return inputOperator(value);
+	if (value.match(/[0-9]/g)) return inputNumber(value);
+}
+// Input functions
+function inputEqual() {
 	//calculate new result
 	const result = operate(
 		firstEnteredNumber,
@@ -32,13 +46,13 @@ function clickedEqual() {
 	isFirstNumberEntered = true;
 	firstEnteredNumber = result;
 	secondEnteredNumber = 0;
-	enteredOperator = "";
+	enteredOperator = "+";
 
 	// update display
 	clearDisplay();
 	renderDisplay(result);
 }
-function clickedClear() {
+function inputClear() {
 	//reset results
 	lastResult = 0;
 	resultVar = 0;
@@ -50,51 +64,39 @@ function clickedClear() {
 	//clear the display
 	clearDisplay();
 }
-function clickedOperator(value) {
+function inputOperator(value) {
 	enteredOperator = value;
 	isFirstNumberEntered = true;
-    clearDisplay()
+	if (secondEnteredNumber !== 0) {
+		inputEqual();
+	}
+	// clearDisplay()
 }
-function clickedNumber(value) {
-	console.log("number: ",value)
-    if(!isFirstNumberEntered){
-        firstEnteredNumber === 0 
-        ? firstEnteredNumber = value
-        : firstEnteredNumber += value
-    }else {
-        secondEnteredNumber === 0 
-        ? secondEnteredNumber = value
-        : secondEnteredNumber += value
-    }
-    let number = isFirstNumberEntered ? secondEnteredNumber : firstEnteredNumber
+function inputNumber(value) {
+	console.log("number: ", value);
+	if (!isFirstNumberEntered) {
+		firstEnteredNumber === 0
+			? (firstEnteredNumber = value)
+			: (firstEnteredNumber += value);
+	} else {
+		secondEnteredNumber === 0
+			? (secondEnteredNumber = value)
+			: (secondEnteredNumber += value);
+	}
+	let number = isFirstNumberEntered
+		? secondEnteredNumber
+		: firstEnteredNumber;
 	renderDisplay(number);
 }
 
-// Click handler
-function handleClick(e) {
-	let value = e.target.dataset.value;
-	console.log(value);
-	if (value === "=") return clickedEqual();
-	if (value === "clear") return clickedClear();
-	if (
-		value === "+" ||
-		value === "-" ||
-		value === "*" ||
-		value === "/" ||
-		value === "**"
-	)
-		return clickedOperator(value);
-	return clickedNumber(value);
-}
-
-//Math Functions
+// Math Functions
 const add = (number1, number2) => number1 + number2;
 const subtract = (number1, number2) => number1 - number2;
 const multiply = (number1, number2) => number1 * number2;
 const divide = (number1, number2) => number1 / number2;
 const power = (number1, number2) => number1 ** number2;
 
-//Perform operations
+// Perform operations
 function operate(number1, number2, operator) {
 	number1 = Number(number1);
 	number2 = Number(number2);
@@ -119,11 +121,10 @@ function operate(number1, number2, operator) {
 	return result;
 }
 
-//Manage display
+// Manage display
 function renderDisplay(number) {
 	return (resultDiv.textContent = number);
 }
 function clearDisplay() {
 	return (resultDiv.textContent = 0);
 }
-clickedClear();
